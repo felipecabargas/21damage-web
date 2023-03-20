44ae3b264b8dbe0d698fad97cac27114098f17ee
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2023-03-20 07:49:13 +0100

    FIX: 6.0.0 deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4bfa2daba423bbda4f3636fd22596aad",
-"index.html": "1a818881104e0e6079d2c2d782e491f2",
-"/": "1a818881104e0e6079d2c2d782e491f2",
+"index.html": "3a28686604bfd7306096e28a32988c70",
+"/": "3a28686604bfd7306096e28a32988c70",
 "main.dart.js": "c90c141cc783afcb67868d0162206779",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
